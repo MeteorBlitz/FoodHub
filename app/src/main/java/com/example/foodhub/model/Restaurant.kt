5c1f874e444b01a63375c2e2e7fd2cd4d7ada629
deepak <dlipkumar77@gmail.com>
2025-04-23 14:11:13 +0530

    update model & dummy data added

--- app/src/main/java/com/example/foodhub/model/Restaurant.kt
+++ app/src/main/java/com/example/foodhub/model/Restaurant.kt
@@ -3,7 +3,7 @@ package com.example.foodhub.model
 data class Restaurant(
     val id: Int,
     val name: String,
-    val imageUrl: String,
-    val description: String,
-    val rating: Double
+    val rating: Double,
+    val location: String,
+    val image_url: String
 )
---
+++ app/src/main/java/com/example/foodhub/model/RestaurantData.kt
@@ -0,0 +1,20 @@
+package com.example.foodhub.model
+
+object RestaurantData {
+    val restaurantList = listOf(
+        Restaurant(
+            id = 1,
+            name = "The Spicy Spoon",
+            rating = 4.5,
+            location = "Trichy",
+            image_url = "https://fastly.picsum.photos/id/63/5000/2813.jpg?hmac=HvaeSK6WT-G9bYF_CyB2m1ARQirL8UMnygdU9W6PDvM"
+        ),
+        Restaurant(
+            id = 2,
+            name = "Tandoori Nights",
+            rating = 4.2,
+            location = "Chennai",
+            image_url = "https://fastly.picsum.photos/id/292/3852/2556.jpg?hmac=cPYEh0I48Xpek2DPFLxTBhlZnKVhQCJsbprR-Awl9lo"
+        )
+    )
+}
