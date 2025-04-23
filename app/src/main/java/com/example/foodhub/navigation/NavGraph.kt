14d8f143e53e0879dd03439d0699ab59a1c5d8be
deepak <dlipkumar77@gmail.com>
2025-04-23 13:24:09 +0530

    navigation added

---
+++ app/src/main/java/com/example/foodhub/navigation/NavGraph.kt
@@ -0,0 +1,19 @@
+package com.example.foodhub.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.foodhub.screens.HomeScreen
+import com.example.foodhub.screens.LoginScreen
+import com.example.foodhub.screens.SplashScreen
+
+@Composable
+fun AppNavGraph(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = Screen.Splash.route) {
+        composable(Screen.Splash.route) { SplashScreen(navController) }
+        composable(Screen.Login.route) { LoginScreen(navController) }
+        composable(Screen.Home.route) { HomeScreen(navController) }
+
+    }
+}
