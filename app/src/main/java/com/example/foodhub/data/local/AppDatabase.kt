3cbd76472f279fd1ceaee190020387fe038e2e40
deepak <dlipkumar77@gmail.com>
2025-04-26 12:10:21 +0530

    room schema warning fix

--- app/src/main/java/com/example/foodhub/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/foodhub/data/local/AppDatabase.kt
@@ -3,7 +3,7 @@ package com.example.foodhub.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [CartItem::class], version = 1)
+@Database(entities = [CartItem::class], version = 1, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun cartDao(): CartDao
 }
